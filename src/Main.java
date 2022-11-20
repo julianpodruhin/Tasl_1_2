e9ec2a8c8337acb729da937586c10fa21326c52a
julianpodruhin <yulianpodruhin@mail.ru>
2022-11-21 02:01:32 +0300

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a= 46;
+        int b= 10;
+        int c= 3;
+        int d= 29;
+        int e= 4;
+        int f= -15;
+        float s_1 =(a + b)*(b / c);
+        int s_2 = d*e*f;
+        System.out.println(s_1);
+        System.out.println(s_2);
+
+
     }
 }
